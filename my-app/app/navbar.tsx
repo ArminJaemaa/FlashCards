4dec116acbfa7a6d05bd6717b26d010950c6d728
ArminJaemaa <ajaemaa@tlu.ee>
2025-10-28 22:58:33 +0200

    feat(navbar): add navigation functionality to navbar

--- my-app/app/navbar.tsx
+++ my-app/app/navbar.tsx
@@ -1,51 +1,47 @@
 "use client";
 
-import { useState } from "react";
 import {
   IconBellRinging,
   IconDatabaseImport,
   IconFingerprint,
   IconHome,
 } from "@tabler/icons-react";
-import { Code, Group } from "@mantine/core";
 import classes from "./navbar.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const data = [
-  { link: "", label: "Home", icon: IconHome },
-  { link: "", label: "FlashCards", icon: IconFingerprint },
-  { link: "", label: "Create flash cards", icon: IconDatabaseImport },
-  { link: "", label: "Overview", icon: IconBellRinging },
+  { link: "/", label: "Home", icon: IconHome },
+  { link: "/flashcards", label: "FlashCards", icon: IconFingerprint },
+  {
+    link: "/flashcards/create",
+    label: "Create flash cards",
+    icon: IconDatabaseImport,
+  },
+  { link: "flasCards/owerview", label: "Overview", icon: IconBellRinging },
 ];
 
 export function NavbarSimpleColored() {
-  const [active, setActive] = useState("Billing");
+  const pathname = usePathname();
 
-  const links = data.map((item) => (
-    <a
-      className={classes.link}
-      data-active={item.label === active || undefined}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
+  const links = data.map((item) => {
+    const isActive = pathname === item.link;
+
+    return (
+      <Link
+        href={item.link}
+        key={item.label}
+        className={`${classes.link} ${isActive ? classes.active : ""}`}
+      >
+        <item.icon className={classes.linkIcon} stroke={1.5} />
+        <span>{item.label}</span>
+      </Link>
+    );
+  });
 
   return (
     <nav className={classes.navbar}>
-      <div className={classes.navbarMain}>
-        <Group className={classes.header} justify="space-between">
-          <Code fw={700} className={classes.version}>
-            v3.1.2
-          </Code>
-        </Group>
-        {links}
-      </div>
+      <div className={classes.navbarMain}>{links}</div>
     </nav>
   );
 }

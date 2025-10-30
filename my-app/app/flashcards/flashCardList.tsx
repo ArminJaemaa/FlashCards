ad784928b64e190deed5c764c3f9ffa6e28eb889
ArminJaemaa <ajaemaa@tlu.ee>
2025-10-30 11:27:21 +0200

    fix: fix question and answer rotation

--- my-app/app/flashcards/flashCardList.tsx
+++ my-app/app/flashcards/flashCardList.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 export interface FlashCard {
   id: number;
   title: string;
-  description: string;
+  answer: string;
   deleted: boolean;
 }
 
@@ -17,16 +17,35 @@ export default function FlashCardList({
 }) {
   if (!flashCards || flashCards.length === 0) return <p>Pole ühtegi</p>;
 
+  const [shownAnswers, setShownAnswers] = useState<Set<number>>(new Set());
+
+  const toggleCard = (id: number) => {
+    setShownAnswers((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(id)) newSet.delete(id);
+      else newSet.add(id);
+      return newSet;
+    });
+  };
+
   return (
-    <ul>
-      {flashCards.map((card) => (
-        <li
-          key={card.id}
-          className="bg-white shadow-sm border border-gray-200 p-4 rounded-lg flex justify-between items-start"
-        >
-          {card.title}
-        </li>
-      ))}
-    </ul>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+      {flashCards.map((card) => {
+        const shownAnswer = shownAnswers.has(card.id);
+        return (
+          <div
+            key={card.id}
+            onClick={() => toggleCard(card.id)}
+            className={`bg-white shadow-lg border border-gray-200 p-6 rounded-lg cursor-pointer`}
+          >
+            <div
+              className={`relative w-full h-40 text-center flex items-center justify-center backface-hidden`}
+            >
+              {shownAnswer ? card.answer : card.title}
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
